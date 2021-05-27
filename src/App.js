c2ba69a9b9f96e8f1f3065e3eac3d19dc453b0c3
shadman17 <shadman.wasif@gmail.com>
2021-05-27 14:06:50 +0600

    updating shop page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from 'react-router-dom';  
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './homepage/homepage/homepage.component'
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1> Hats Page </h1>
-    </div>
-  )
-}
+import HomePage from "./homepage/homepage/homepage.component";
+import ShopPage from "./homepage/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component = {HomePage}/>
-      <Route path='/hats' component = {HatsPage}/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+
       {/*<HomePage />*/}
     </div>
   );
