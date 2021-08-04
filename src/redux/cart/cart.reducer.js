2e0204aa92bd489976145d7aa5373959a0babe21
shadman17 <shadman.wasif@gmail.com>
2021-08-04 13:40:09 +0600

    Added Cart Functionality using redux(can be viewed in console)

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,19 +1,27 @@
 import CartActionTypes from './cart.types';
 
+import { addItemToCart } from './cart.utils'
+
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return {
-        ...state,
-        hidden: !state.hidden
-      };
-    default:
-      return state;
+  if (action.type === CartActionTypes.TOGGLE_CART_HIDDEN){
+    return {
+      ...state,
+      hidden: !state.hidden
+    };
   }
+  else if (action.type === CartActionTypes.ADD_ITEM) {
+    return {
+      ...state,
+      cartItems: addItemToCart(state.cartItems, action.payload)
+    }
+  }
+  else
+    return state;
 };
 
 export default cartReducer;
